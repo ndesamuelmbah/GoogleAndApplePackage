47b9333560a1c5ad86a9a4697c9e81c4c8d38fbc
Samuel Nde <ndesamuelmbah@gmail.com>
2022-11-20 23:19:45 -0800

    Minor Change

--- pay_android/lib/src/widgets/google_pay_button.dart
+++ pay_android/lib/src/widgets/google_pay_button.dart
@@ -11,7 +11,6 @@
 /// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
-
 part of '../../pay_android.dart';
 
 /// The types of button supported on Google Pay.
